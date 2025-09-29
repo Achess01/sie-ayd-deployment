cdbbf1c9d630900d6dc4a2e51ad47bee710ecfe2
Alexander Tzoc <jealtzo26@gmail.com>
2025-09-28 23:52:58 -0600

    Front deployment

---
+++ jenkins/pipelines/frontend-pipeline.groovy
@@ -0,0 +1,52 @@
+pipeline {
+    agent any
+
+    environment {
+        DOCKER_COMPOSE_FILE = 'docker-compose.yml'
+        FRONTEND_REPO = 'git@github.com:Achess01/sie-ayd-frontend.git'
+        COMPOSE_PROJECT_NAME = 'sie-ayd-deployment'
+    }
+
+    stages {
+        stage('Checkout Deployment Repo') {
+            steps {
+                checkout scm
+            }
+        }
+
+        stage('Checkout Frontend Code') {
+            steps {
+                dir('frontend') {
+                    git branch: 'main', url: "${FRONTEND_REPO}", credentialsId: 'ssh-jenkins'
+                }
+            }
+        }
+
+        stage('Build Docker Images') {
+            steps {
+                sh 'docker compose -p ${COMPOSE_PROJECT_NAME} build --no-cache frontend'
+            }
+        }
+
+        stage('Deploy Application') {
+            steps {
+                sh 'docker compose -p ${COMPOSE_PROJECT_NAME} stop frontend || true'
+                sh 'docker compose -p ${COMPOSE_PROJECT_NAME} rm -f frontend || true'
+                sh 'docker compose -p ${COMPOSE_PROJECT_NAME} up -d frontend'
+            }
+        }
+
+    }
+    
+    post {
+        always {
+            sh 'docker compose -p ${COMPOSE_PROJECT_NAME} logs frontend || true'
+        }
+        failure {
+            sh 'docker compose -p ${COMPOSE_PROJECT_NAME} logs frontend || true'
+        }
+        success {
+            sh 'echo "Deployment successful!"'
+        }
+    }
+}
